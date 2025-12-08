05eb2130da024ce9dfa2b8b952125ebe0b6e3e19
sacmanagement7-crypto <sacmanagement7@gmail.com>
2025-12-08 18:20:33 +0530

    main

--- src/dummydata/magzine.js
+++ src/dummydata/magzine.js
@@ -1,8 +1,10 @@
 // Import PDFs from assets folder
 import pdf1 from "../assets/pdf1formagzine1.pdf"
+import pdf2 from "../assets/pdf2formagzine2.pdf"
 
 // Optional: import cover pages from assets
 import cover1 from "../assets/magzine1.png";
+import cover2 from "../assets/magzine2.png"
 
 // Dummy magazines array
 export const magazines = [
@@ -11,6 +13,14 @@ export const magazines = [
     title: "अंक 1",
     titlePage: cover1,                   // Imported from assets
     pdf: pdf1,                           // Imported from assets
+    banner: "/banners/banner1.jpg",      // Public folder
+    isSubscribed: true,
+  },
+   {
+    _id: "102",
+    title: "अंक 2",
+    titlePage: cover2,                   // Imported from assets
+    pdf: pdf2,                           // Imported from assets
     banner: "/banners/banner1.jpg",      // Public folder
     isSubscribed: true,
   },
